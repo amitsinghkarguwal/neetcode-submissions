b0d7d894fe6151223724e9dcd7765443415652f7
Amit Singh <111610634+amitsinghkarguwal@users.noreply.github.com>
2026-08-17 22:13:50 +0530

    Add: island-perimeter - submission-2

---
+++ Data Structures & Algorithms/island-perimeter/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;  //empty method works for vector also.
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int perimeter = 0;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                // If we land on an island cell
+                if (grid[r][c] == 1) {
+                    // Assume it's completely isolated first
+                    perimeter += 4;
+                    
+                    // Check if there is land ABOVE it
+                    if (r > 0 && grid[r - 1][c] == 1) {
+                        perimeter -= 2; // Subtract the shared horizontal edge
+                    }
+                    
+                    // Check if there is land to the LEFT of it
+                    if (c > 0 && grid[r][c - 1] == 1) {
+                        perimeter -= 2; // Subtract the shared vertical edge
+                    }
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+};
